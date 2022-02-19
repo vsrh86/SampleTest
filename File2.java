ce85370648576fb2aba8795d6c343e44c313699d
jenkins <vsrh86@gmail.com>
2022-02-19 22:11:41 +0530

    Added chnages

--- File2.java
+++ File2.java
@@ -7,16 +7,6 @@ import java.util.stream.Collectors;
 
 public class CheckDuplicate {
 public static void main(String[] args) {
-	int num[]= {13,33,13,23,54,65};
-	//Using hashset Method
-	HashSet uniqueelements= new HashSet();
-	Set s=Arrays.stream(num).filter(i->!uniqueelements.add(i)).boxed()
-	.collect(Collectors.toSet());
-	System.out.println(s);
-	
-//	Map hm= new HashMap(l);
-	
-	
-	
+	System.out.println("File2===");
 			}
 }
